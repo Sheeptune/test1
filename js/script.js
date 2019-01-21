5e01950effda1cf24ca4399688d98f811d618bb0
philippe <philippe@scil.coop>
2019-01-21 18:06:29 +0100

    progress bar pending

--- js/script.js
+++ js/script.js
@@ -3,3 +3,17 @@
 $(function () {
     $('[data-toggle="popover"]').popover()
   })
+
+  // Progress bar management
+  // Activite auto-width
+
+    $(".progress-bar").each(function(){
+      each_bar_width = $(this).attr('aria-valuenow');
+      $(this).width(each_bar_width + '%');
+    });
+
+    $(".indicator").each(function(){
+      console.log($(this))
+      indicator_left = $(this).attr('aria-valuenow');
+      $(this).css('left', 'calc(' + indicator_left + '% - 0.5em)');
+    });
